346fdfc006fc9acf48d298c07d627462612e580a
henriquewallace <henrique.wallace@hotmail.com>
2023-11-08 22:18:49 -0300

    feat(server): create CodeVerifierUtil utility class

---
+++ server/src/main/java/com/syncsoundz/server/util/CodeVerifierUtil.java
@@ -0,0 +1,15 @@
+package com.syncsoundz.server.util;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class CodeVerifierUtil {
+
+    public static String generate() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] codeVerifier = new byte[32];
+        secureRandom.nextBytes(codeVerifier);
+
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(codeVerifier);
+    }
+}
