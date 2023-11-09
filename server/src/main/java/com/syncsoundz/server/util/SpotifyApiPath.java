bcfd9969c4159d035ffb65621e4ae05a767d45c6
henriquewallace <henrique.wallace@hotmail.com>
2023-11-08 22:54:33 -0300

    feat(server): create SpotifyApiPath class to add paths consts

---
+++ server/src/main/java/com/syncsoundz/server/util/SpotifyApiPath.java
@@ -0,0 +1,6 @@
+package com.syncsoundz.server.util;
+
+public class SpotifyApiPath {
+    public static final String REQUEST_AUTHORIZATION = "/authorize";
+    public static final String ACCESS_TOKEN = "/api/token";
+}
