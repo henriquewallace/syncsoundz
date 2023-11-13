b8108b1622f9eb314515f3d88081fd88b8434fd2
henriquewallace <henrique.wallace@hotmail.com>
2023-11-13 20:30:10 -0300

    feat(server): create getAccessToken auth controller

--- server/src/main/java/com/syncsoundz/server/controller/v1/AuthController.java
+++ server/src/main/java/com/syncsoundz/server/controller/v1/AuthController.java
@@ -2,13 +2,19 @@ package com.syncsoundz.server.controller.v1;
 
 import com.syncsoundz.server.domain.AuthRedirectUrl;
 import com.syncsoundz.server.domain.AuthRequest;
+import com.syncsoundz.server.domain.AuthRequestToken;
+import com.syncsoundz.server.domain.AuthResponseToken;
 import com.syncsoundz.server.service.AuthService;
 import com.syncsoundz.server.util.CodeChallengeUtil;
 import com.syncsoundz.server.util.CodeVerifierUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClientException;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -19,28 +25,54 @@ public class AuthController {
 
     @Value("${REDIRECT_URI_GET_TOKEN}")
     private String redirectUrlGetToken;
+
+    private String codeVerifier;
+
     private final AuthService authService;
+    private final AuthRequest authRequest;
 
     public AuthController(AuthService authService) {
         this.authService = authService;
+        this.authRequest = new AuthRequest();
+        authRequest.setCode_challenge_method("S256");
+        authRequest.setResponse_type("code");
+        authRequest.setScope("user-read-private user-read-email");
     }
 
     @GetMapping("/login")
     public AuthRedirectUrl requestUserAccess() {
-        String codeVerifier = CodeVerifierUtil.generate();
+        codeVerifier = CodeVerifierUtil.generate();
         String codeChallenge = CodeChallengeUtil.generate(codeVerifier);
 
-        AuthRequest authRequest = new AuthRequest();
-        authRequest.setClient_id(clientId);
-        authRequest.setCode_challenge_method("S256");
         authRequest.setCode_challenge(codeChallenge);
-        authRequest.setResponse_type("code");
+        authRequest.setClient_id(clientId);
         authRequest.setRedirect_uri(redirectUrlGetToken);
-        authRequest.setScope("user-read-private user-read-email");
 
         AuthRedirectUrl authRedirectUrl = new AuthRedirectUrl();
         authRedirectUrl.setAuthRedirectUrl(authService.requestUserAuthorization(authRequest));
 
         return authRedirectUrl;
+    }
+
+    @GetMapping(value = "/get-token", produces = MediaType.TEXT_HTML_VALUE)
+    public String getAccessToken(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
+        AuthRequestToken authRequestToken = new AuthRequestToken();
+        authRequestToken.setCode(code);
+        authRequestToken.setCode_verifier(codeVerifier);
+        authRequestToken.setRedirect_uri(authRequest.getRedirect_uri());
+        authRequestToken.setClient_id(authRequest.getClient_id());
+        authRequestToken.setGrant_type("authorization_code");
+
+        AuthResponseToken authResponseToken = new AuthResponseToken();
+        try {
+            authResponseToken.setAccess_token(authService.getAccessToken(authRequestToken).getAccess_token());
+
+        } catch (WebClientException error) {
+            System.out.println("Error: " + error.getMessage());
+        }
+
+        response.sendRedirect("http://localhost:3000");
+
+        return authResponseToken.getAccess_token();
     }
 }
